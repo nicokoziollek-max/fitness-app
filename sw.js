419469e84823842a81b649c6a2efcea974742d3d
nicokoziollek-max <nico.koziollek@gmail.com>
2026-05-26 23:04:46 +0200

    Add client profile photo avatar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nh-training-v4";
+const CACHE = "nh-training-v5";
 const ASSETS = [
   "./",
   "./index.html",
@@ -6,6 +6,7 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./assets/icon.svg",
+  "./assets/profile-nicolas.jpg",
   "./data/fitness-data.js",
 ];
 
